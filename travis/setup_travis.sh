2ea100e53800b37e77a143e0974860ea92dda2d5
Eric Prestat <eric.prestat@gmail.com>
2019-09-12 16:31:10 +0100

    Fix azure and script path on travis.

--- travis/setup_travis.sh
+++ travis/setup_travis.sh
@@ -1,10 +1,11 @@
+DIRECTORY="${BASH_SOURCE%/*}/"
 # Download and install miniconda distribution
 if [ $ENV == conda ]; then
-  source ./travis_setup_conda_$TRAVIS_OS_NAME.sh;
+  source $DIRECTORY/travis_setup_conda_$TRAVIS_OS_NAME.sh;
 fi
 
 # Setup environment
 if [[ $MINIMAL_ENV == false ]] ; then
   DEPS="${DEPS} ${DEPS_OPTIONAL}";
 fi
-source ./travis_install_with_$ENV.sh
+source $DIRECTORY/travis_install_with_$ENV.sh
