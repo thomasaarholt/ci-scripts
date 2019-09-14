2c9760dcf4264a4c3606d9da2d255197c247e079
Eric Prestat <eric.prestat@gmail.com>
2019-09-14 10:16:45 +0100

    source call inside the script file is not working on azure, try a different approach.

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -2,7 +2,4 @@ conda update -y conda
 conda config --append channels conda-forge
 conda create --yes --quiet --name test_env python=$PYTHON_VERSION
 conda env update --name test_env --file conda_environment.yml
-conda activate testenv
-conda install -y $DEPS $TEST_DEPS
-conda info
-conda list
+conda env update --name test_env --file conda_environment_dev.yml
