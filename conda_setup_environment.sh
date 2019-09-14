621593f5e8afe0e84bb0dca7fffd1ad76650633f
Eric Prestat <eric.prestat@gmail.com>
2019-09-14 10:53:30 +0100

    Use variable for conda environment name.

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -1,5 +1,7 @@
 conda update -y conda
 conda config --append channels conda-forge
-conda create --yes --quiet --name test_env python=$PYTHON_VERSION
-conda env update --name test_env --file conda_environment.yml
-conda env update --name test_env --file conda_environment_dev.yml
+conda create --yes --quiet --name $ENV_NAME python=$PYTHON_VERSION
+conda env update --name $ENV_NAME --file conda_environment.yml
+conda env update --name $ENV_NAME --file conda_environment_dev.yml
+conda info
+conda list -n $ENV_NAME
