b2ddedcce6bc615389201b9a2e7054ec5bee91de
Eric Prestat <eric.prestat@gmail.com>
2019-09-15 12:31:17 +0100

    Fix environment variable to set conda python version accross all CIs and rename it to CONDA_PYTHON_VERSION.

--- conda_setup_environment.sh
+++ conda_setup_environment.sh
@@ -1,6 +1,6 @@
 conda update -y conda
 conda config --append channels conda-forge
-conda create --yes --quiet --name $ENV_NAME python=$PYTHON_VERSION
+conda create --yes --quiet --name $ENV_NAME python=$CONDA_PYTHON_VERSION
 conda env update --name $ENV_NAME --file conda_environment.yml
 conda env update --name $ENV_NAME --file conda_environment_dev.yml
 conda info
